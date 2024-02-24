3780c85cab6c6022d19984a3528075c238ceb295
rashmiKumari03 <rashmi15497@gmail.com>
2024-02-24 22:36:04 +0530

    Data in Mysql

---
+++ 02.Student_Performance_ML_E2E_project/MySQL_data.sql
@@ -0,0 +1,8 @@
+create database student;
+
+# Lets upload the dataset manually here in the MySQL.
+use student;
+select * from student_info;
+
+
+
---
+++ 02.Student_Performance_ML_E2E_project/src/student_performace_MLProject/components/data_ingestion.py
@@ -0,0 +1,3 @@
+
+# Database->data->train_test_split_format
+# Get the Data in MySQL Database and read the data from MySQL .
