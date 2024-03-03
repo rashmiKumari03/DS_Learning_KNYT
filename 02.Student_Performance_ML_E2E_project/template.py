4688dd86a699e6e0d971f59c4ad680d2937566a8
rashmiKumari03 <rashmi15497@gmail.com>
2024-03-03 21:15:28 +0530

    update

--- 02.Student_Performance_ML_E2E_project/template.py
+++ 02.Student_Performance_ML_E2E_project/template.py
@@ -12,7 +12,6 @@ list_of_files=[
     f"src/{project_name}/components/data_ingestion.py",
     f"src/{project_name}/components/data_transformation.py",
     f"src/{project_name}/components/model_trainer.py",
-    f"src/{project_name}/components/model_monitoring.py",
 
     f"src/{project_name}/pipelines/__init__.py",
     f"src/{project_name}/pipelines/training_pipeline.py",
