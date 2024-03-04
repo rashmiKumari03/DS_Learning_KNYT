7808939f9a4abe57b78c57e83a830d5e99b2cc1b
rashmiKumari03 <rashmi15497@gmail.com>
2024-03-05 03:57:07 +0530

    commit

--- 02.Student_Performance_ML_E2E_project/main_app.py
+++ 02.Student_Performance_ML_E2E_project/main_app.py
@@ -35,7 +35,7 @@ def predict_datapoint():
         )
 
         pred_df = data.get_data_as_DataFrame()
-        print("prediction dataframe:", pred_df)
+        print("prediction dataframe:\n", pred_df)
 
         predict_pipeline = PredictPipeline()
         result = predict_pipeline.predict(pred_df)
@@ -44,4 +44,4 @@ def predict_datapoint():
         return render_template("home.html", result=result[0])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8000)
+    app.run(host="0.0.0.0",port=8000,debug=True)
